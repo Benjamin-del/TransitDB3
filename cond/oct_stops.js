869e4f141347dafaa7e8d869fc90cee23f169588
Benjamin-del <benjamin.maheral@gmail.com>
2024-09-05 11:46:55 -0400

    Fix DataType Change error & Update Prisma

--- cond/oct_stops.js
+++ cond/oct_stops.js
@@ -7,15 +7,15 @@ module.exports = {
             throw new Error("UPDATE (TRIPS): ERROR: " + times)
         }
         return stops.filter((x) => {
-            return x !== ""
+            return x !== "" || x.split(",")[0] !== "stop_id"
         }).map(x => {
             const dts = x.split(",")
             return {
                 stop_id: dts[0],
                 stop_code: dts[1],
                 stop_name: dts[2],
-                stop_lat: dts[5],
-                stop_lon: dts[6]
+                stop_lat: Number(dts[5]),
+                stop_lon: Number(dts[6])
             }
         })
     }
